b282eacd85d9afccbb8ff4bee753a4ffbae36f14
Andrej Katin <katin.andrej96@gmail.com>
2025-09-24 08:18:07 +0200

    Promena algoritma za heširanje lozinki.

--- TransactionApi/Utils/HashFunctions.cs
+++ TransactionApi/Utils/HashFunctions.cs
@@ -5,6 +5,15 @@ namespace TransactionApi.Utils
 {
     public static class HashFunctions
     {
+        public static string ComputeSha256Hash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                var hash = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                return BitConverter.ToString(hash).Replace("-", "").ToLower();
+            }
+        }
+
         public static string ComputeMd5Hash(string rawData)
         {
             using (MD5 md5Hash = MD5.Create())
